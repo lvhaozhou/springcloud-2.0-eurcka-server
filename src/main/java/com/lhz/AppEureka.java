bacf0da20618b73260547a35a5e3731365536331
lhz <lvhaozhou@zxint.com>
2019-12-20 11:23:07 +0800

    springcloud eureka注册中心第一次提交

---
+++ src/main/java/com/lhz/AppEureka.java
@@ -0,0 +1,21 @@
+package com.lhz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Auther: lhz
+ * @Date: 2019/12/7 0007 14:12
+ * @Description: 注册中心启动类
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class AppEureka {
+    //@EnableEurekaServer 表示开启注册中心
+    public static void main(String[] args) {
+        SpringApplication.run(AppEureka.class, args);
+    }
+}
+
